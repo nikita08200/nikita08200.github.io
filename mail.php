a2e46b8d675a385e090e3dd9e7a009e387c7f9a2
nikita08200 <nikita1bogdan@gmail.com>
2017-07-21 19:57:20 +0200

    third

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+//Принимаем постовые данные
+$username=$_POST['username'];
+$email=$_POST['email'];
+$user_message=$_POST['user_message'];
+//Тут указываем на какой ящик посылать письмо
+$to = "nikita1bogdan@gmail.com";
+//Далее идет тема и само сообщение
+// Тема письма
+$subject = "Заявка с сайта";
+// Сообщение письма
+$message = "
+Форма которую заполнил клиент: ".htmlspecialchars($whatever)."<br />
+Имя пользователя: ".htmlspecialchars($username)."<br />
+Email: ".htmlspecialchars($email)."<br />
+Сообщение: ".htmlspecialchars($user_message);
+// Отправляем письмо при помощи функции mail();
+$headers = "From: https://nikita08200.github.io <nikita61511@gmail.com>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail ($to, $subject, $message, $headers);
+// Перенаправляем человека на страницу благодарности и завершаем скрипт
+header('Location: https://nikita08200.github.io');
+exit();
+?>
